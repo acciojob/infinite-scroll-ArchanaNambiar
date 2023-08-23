fe741b4930c8a85541df039d0c6c2d980a659285
ArchanaNambiar <64783368+ArchanaNambiar@users.noreply.github.com>
2023-08-23 20:45:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 //your code here!
 
+// Get the list element
+const infiList = document.getElementById('infi-list');
+
+// Function to create and append a new list item
+function createListItem(itemNumber) {
+  const li = document.createElement('li');
+  li.textContent = `List Item ${itemNumber}`;
+  infiList.appendChild(li);
+}
+
+// Function to handle scrolling and load more items
+function handleScroll() {
+  const scrollTop = infiList.scrollTop;
+  const scrollHeight = infiList.scrollHeight;
+  const clientHeight = infiList.clientHeight;
+
+  if (scrollTop + clientHeight >= scrollHeight) {
+    loadMoreItems();
+  }
+}
+
+// Function to load more items
+let currentItemNumber = 11; // Start from the 11th item
+function loadMoreItems() {
+  for (let i = 0; i < 2; i++) {
+    createListItem(currentItemNumber);
+    currentItemNumber++;
+  }
+}
+
+// Initial load of default items
+for (let i = 1; i <= 10; i++) {
