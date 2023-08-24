d8e806dc0e7c18b1d0e823206123da69504631fd
ArchanaNambiar <64783368+ArchanaNambiar@users.noreply.github.com>
2023-08-24 22:41:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-//your code here!
+const list = document.getElementById('infi-list');
+const initialItemsCount = 10;
+let currentItemsCount = initialItemsCount;
 
-// Get the list element
-const infiList = document.getElementById('infi-list');
-
-// Function to create and append a new list item
-function createListItem(itemNumber) {
-  const li = document.createElement('li');
-  li.textContent = `List Item ${itemNumber}`;
-  infiList.appendChild(li);
+// Function to add list items
+function addListItems(count) {
+  for (let i = 1; i <= count; i++) {
+    const li = document.createElement('li');
+    li.textContent = `List Item ${currentItemsCount + i}`;
+    list.appendChild(li);
+  }
+  currentItemsCount += count;
 }
 
-// Function to handle scrolling and load more items
-function handleScroll() {
-  const scrollTop = infiList.scrollTop;
-  const scrollHeight = infiList.scrollHeight;
-  const clientHeight = infiList.clientHeight;
+// Add initial list items
+addListItems(initialItemsCount);
+
+// Function to check if user reached the end of the list
+function checkScroll() {
+  const scrollTop = list.scrollTop;
+  const scrollHeight = list.scrollHeight;
+  const clientHeight = list.clientHeight;
 
   if (scrollTop + clientHeight >= scrollHeight) {
-    loadMoreItems();
+    // Add 2 more list items when reaching the end
+    addListItems(2);
   }
 }
 
-// Function to load more items
-let currentItemNumber = 11; // Start from the 11th item
-function loadMoreItems() {
-  for (let i = 0; i < 2; i++) {
-    createListItem(currentItemNumber);
-    currentItemNumber++;
-  }
-}
-
-// Initial load of default items
-for (let i = 1; i <= 10; i++) {
+// Listen for scroll events
+list.addEventListener('scroll', checkScroll);
